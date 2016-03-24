fb715a65a667f38c25476d2d7f161028101e540b
unknown <ayl292@nyu.edu>
2016-03-24 16:46:08 -0400

    somewhat working maze, TODO: fix cat raycast

---
+++ Assets/Scripts/cat.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class cat : MonoBehaviour {
+
+	public Transform mouse;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(mouse == null){
+			return;
+		}
+		else{
+			Vector3 directionToMouse = mouse.position - transform.position;
+			float angle = Vector3.Angle(transform.forward, directionToMouse);
+
+			if(angle < 90){
+				Ray catRay = new Ray(transform.position, directionToMouse);
+				RaycastHit catRayHitInfo = new RaycastHit();
+				//Ray is not drawing fix it 
+				Debug.DrawRay(transform.position, directionToMouse * 1000f, Color.yellow);
+
+				if(Physics.Raycast(catRay, out catRayHitInfo, 100f)){
+					if(catRayHitInfo.collider.tag == "Mouse"){
+						if(catRayHitInfo.distance <= 5){
+							Destroy(mouse);
+						}
+						else{
+							GetComponent<Rigidbody>().AddForce(directionToMouse.normalized * 1000f);
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/mouse.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouse : MonoBehaviour {
+
+	public Transform cat; 
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+		Vector3 directionToCat = cat.position - transform.position;
+		float angle = Vector3.Angle(transform.forward, directionToCat);
+
+		if(angle < 180){
+			Ray mouseRay = new Ray(transform.position, directionToCat);
+			RaycastHit mouseRayHitInfo = new RaycastHit();
+			//Debug.DrawRay(transform.position, directionToCat * 1000f, Color.yellow);
+
+			if(Physics.Raycast(mouseRay, out mouseRayHitInfo, 100f)){
+				if(mouseRayHitInfo.collider.tag == "Cat"){
+					//Debug.Log("ggwp");
+					GetComponent<Rigidbody>().AddForce(-directionToCat.normalized * 1000f);
+				}
+			}
+		}
+	}
+}
